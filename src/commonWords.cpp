18f9f81e30a90d653f24f6dedd12363560aee6f2
anushaprem29 <anushaprem29@missionrnd.com>
2016-03-18 21:49:46 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -13,9 +13,44 @@ NOTES: If there are no common words return NULL.
 
 #include <stdio.h>
 #include <malloc.h>
-
+#include<string.h>
 #define SIZE 31
+int noOfWords = 0;
+char output[SIZE][SIZE];
+int ifAvaiable(char *str1, char *str2){
+	int iter, count = 0;
+	for (iter = 0; str1[iter] != NULL; iter++){
+		if (str1[iter] == str2[count])
+			count++;
+		else
+			count = 0;
+		if (count == strlen(str2))	return 1;
+	}
+	return 0;
+}
 
+void check(char *str1, char *str2, int start, int end){
+	char *str = (char *)malloc(sizeof(char) *(end - start + 1));
+	int iter;
+	for (iter = start; iter <end; iter++){
+		str[iter - start] = str2[iter];
+	}
+	str[iter] = '\0';
+	if (ifAvaiable(str1, str)){
+		strcpy(output[noOfWords], str);
+		noOfWords++;
+	}
+
+}
 char ** commonWords(char *str1, char *str2) {
-	return NULL;
+	int iter, start = 0;
+	if (str1 == NULL || str2 == NULL)	return NULL;
+	for (iter = 0;; iter++){
+		if (str2[iter] == ' ' || str2[iter] == NULL){
+			check(str1, str2, start, iter);
+			start = iter + 1;
+			if (str2[iter] == NULL)	break;
+		}
+	}
+	return (char**)output;
 }
